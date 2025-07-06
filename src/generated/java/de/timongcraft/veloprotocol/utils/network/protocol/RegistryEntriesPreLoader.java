0cb0426486e8b20e6d96182c6d33d3c57e71c0fc
Timon Seidel <timong.seidel@gmail.com>
2025-07-06 21:51:44 +0200

    feat: pre-loader for generated types classes

---
+++ src/generated/java/de/timongcraft/veloprotocol/utils/network/protocol/RegistryEntriesPreLoader.java
@@ -0,0 +1,41 @@
+package de.timongcraft.veloprotocol.utils.network.protocol;
+
+@SuppressWarnings("unused")
+public class RegistryEntriesPreLoader {
+
+    public static void loadAdvancementTriggers() {
+        load("de.timongcraft.veloprotocol.network.protocol.advancements.AdvancementTrigger");
+    }
+
+    public static void loadVeloSounds() {
+        load("de.timongcraft.veloprotocol.network.protocol.sounds.VeloSounds");
+    }
+
+    public static void loadVeloItemTypes() {
+        load("de.timongcraft.veloprotocol.network.protocol.items.VeloItemTypes");
+    }
+
+    public static void loadVeloEntityEffects() {
+        load("de.timongcraft.veloprotocol.network.protocol.effects.VeloEntityEffects");
+    }
+
+    public static void loadVeloBlockEntityTypes() {
+        load("de.timongcraft.veloprotocol.network.protocol.blockentities.VeloBlockEntityTypes");
+    }
+
+    public static void loadVeloDataComponentTypes() {
+        load("de.timongcraft.veloprotocol.network.protocol.datacomponents.VeloDataComponentTypes");
+    }
+
+    private static void load(String name) {
+        try {
+            Class.forName(name);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private RegistryEntriesPreLoader() {}
+
+}
